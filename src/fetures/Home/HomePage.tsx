f406f709563d44aa083dd13ebd1cfd0a13847ad5
Your Name <youremail@example.com>
2025-08-15 02:07:09 +0600

    feat: add HomePage component with layout structure and integrated sections

---
+++ src/fetures/Home/HomePage.tsx
@@ -0,0 +1,23 @@
+import CategoryPage from "@/component/CategoryPage/CategoryPage";
+import ChepSection from "@/component/ChepSection/ChepSection";
+import HeroSection from "@/component/HeroSection/HeroSection";
+import InstragramPage from "@/component/InstragramPage/InstragramPage";
+import RecipesList from "../AllRecepis/pages/RecipesList";
+
+
+
+const HomePage = () => {
+    return (
+        <div className="border border-gray-200">
+            <HeroSection />
+            <CategoryPage/>
+            <ChepSection/>
+            <InstragramPage/>
+            <RecipesList/>
+            
+        </div>
+    );
+};
+
+export default HomePage;
+

564b4b4ffb2edf4a25ea2923a928701b055cc51a
Your Name <youremail@example.com>
2025-08-15 02:06:56 +0600

    feat: implement AOS for scroll animations and enhance layout behavior on route changes

--- src/layout/MainLayOut.tsx
+++ src/layout/MainLayOut.tsx
@@ -1,9 +1,26 @@
 import Footer from '@/component/Footer/Footer';
 import Navbar from '@/component/Navbar/Navbar';
-import { Outlet } from 'react-router';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router';
+import AOS from 'aos';
+import "aos/dist/aos.css";
 
 
 const MainLayOut = () => {
+ const location = useLocation();
+
+  useEffect(() => {
+    AOS.init({ duration: 800, once: false });
+  }, []);
+
+  useEffect(() => {
+    AOS.refresh();
+  }, [location]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+  
     return (
         <div>
             <Navbar />
@@ -16,3 +33,6 @@ const MainLayOut = () => {
 };
 
 export default MainLayOut;
+
+
+
